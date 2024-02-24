34b95c425cb5a999f32a6b4598d9d0a0cb472b28
sudip <sudipbasakk1234@gmail.com>
2024-02-24 09:52:42 +0530

    created class

--- Basic.java
+++ Basic.java
@@ -1,5 +1,25 @@
 class Basic{
     public static void main(String[] args){
-        System.out.println("hellow");
+        
+
+        // create obj on this Student class
+        // new Student is dynamicialy allocated and return referance
+        //   Student sudip;  static type allocated in stack
+        //so, initial it
+         Student sudip = new Student();
+                sudip.rno=10;
+                sudip.name="sudip";
+                
+         // print
+         System.out.println(sudip.rno);
+         System.out.println(sudip.name);
     }
+
+    //create a class student
+   static class Student{
+        int rno;
+        String name;
+        float cgpa;
+    }
+
 }
